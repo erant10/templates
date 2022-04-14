572e2e581f0073ca19be71c49adaf27c64db9fbe
Eran Toledano <erant10@gmail.com>
2022-04-14 16:34:56 -0700

    Update sync.ps1

--- sync.ps1
+++ sync.ps1
@@ -3,6 +3,8 @@ $OrganizationName = "sentinel-eco-devs"
 $header = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($AzureDevOpsPAT)")) }
 $collectionUri = "https://dev.azure.com/$OrganizationName"
 $branchName = "main"
+$projectId = "fd0310c0-5d21-494b-abcd-1e7bb312c773"
+$repositoryName = "Templates"
 
 function GetBranchObjectId {
     $body = @{filterContains = $branchName}
