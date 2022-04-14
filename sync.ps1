dde9aadb3942bc5eaea30e54034c2dbc0a62c4be
Eran Toledano <erant10@gmail.com>
2022-04-14 15:59:52 -0700

    Update sync.ps1

--- sync.ps1
+++ sync.ps1
@@ -1,6 +1,9 @@
-[CmdletBinding()]
-param (
-  $testArg
-)
-Write-Output "Executing sync script $testArg"
-Write-Output "token " + $Env:devopsToken
+$AzureDevOpsPAT = $Env:devopsToken
+$OrganizationName = "sentinel-eco-devs"
+
+$AzureDevOpsAuthenicationHeader = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($AzureDevOpsPAT)")) }
+
+$UriOrga = "https://dev.azure.com/$($OrganizationName)/" 
+$uriAccount = $UriOrga + "_apis/projects?api-version=5.1"
+
+Invoke-RestMethod -Uri $uriAccount -Method get -Headers $AzureDevOpsAuthenicationHeader 
